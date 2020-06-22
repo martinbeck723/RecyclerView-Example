6a13d5c0adc980075e4cbac7c75538ff76b54611
martinbeck723 <xishuoliu514@gmail.com>
2020-06-22 10:13:22 -0400

    add some method in adapter

--- app/src/main/java/com/example/recyclerviewexample/ContactsRecViewAdapter.java
+++ app/src/main/java/com/example/recyclerviewexample/ContactsRecViewAdapter.java
@@ -1,5 +1,6 @@
 package com.example.recyclerviewexample;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -12,18 +13,24 @@ import java.util.ArrayList;
 public class ContactsRecViewAdapter extends RecyclerView.Adapter<ContactsRecViewAdapter.ViewHolder>{
     private ArrayList<Contact> contacts=new ArrayList<>();
 
+    //constructor
     public ContactsRecViewAdapter() {
     }
+
+    //three mandatory menthod onCreateViewHolder, onBindViewHolder, and getItemCount
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contacts_list_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
+    //most important method
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.txtName.setText(contacts.get(position).getName());
     }
 
     @Override
@@ -33,11 +40,14 @@ public class ContactsRecViewAdapter extends RecyclerView.Adapter<ContactsRecView
 
     public void setContacts(ArrayList<Contact> contacts) {
         this.contacts = contacts;
+        //notify changed so we will refresh
         notifyDataSetChanged();
     }
 
+    //convention to have a ViewHolder inner class
     public class ViewHolder extends RecyclerView.ViewHolder{
         private TextView txtName;
+        //inner class constructor
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txtName=itemView.findViewById(R.id.txtName);
--- app/src/main/java/com/example/recyclerviewexample/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView contactsRecView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +18,12 @@ public class MainActivity extends AppCompatActivity {
         contactsRecView=findViewById(R.id.contactRecView);
         //create our own adapter
 
+        ArrayList<Contact> contacts =new ArrayList<>();
+        //normally this come from a database
+        contacts.add(new Contact("Jay Chow", "jaychow@qq.com", "https://www.channelnewsasia.com/image/11905830/1x1/600/600/8e5dea038bcf0f64c311c5ee3c72082b/YE/jay-chou-love-in-the-sky-hero.jpg"));
+        contacts.add(new Contact("JJ Lin", "jjlin@qq.com", "https://archive.shine.cn/newsimage/2018/02/09/020180209201044.png"));
+        contacts.add(new Contact("Jolin Tsai", "jolin@qq.com", "https://img.ltn.com.tw/Upload/liveNews/BigPic/600_phpaVpGPO.jpg"));
+        contacts.add(new Contact("Ronghao Li", "ronghao@qq.com", "https://youngbean214.files.wordpress.com/2015/03/1.jpg?w=640"));
+        contacts.add(new Contact("Rainie Yang", "rainie@qq.com", "https://www.8days.sg/image/12655004/16x9/1920/1080/a56d46b41123c96792efcacebed1b3f6/gn/ry1.jpg"));
     }
 }
